8409845336d62567de5675c0bed085ab54cab730
BjChacha <bjchacha@outlook.com>
2018-08-01 18:27:27 +0800

    test

---
+++ test.py
@@ -0,0 +1,12 @@
+from pydota import Player, Team, Building
+
+def main():
+    p1 = Player("Chacha", 100, 1)
+    p2 = Player("Sstylle", 1, 2)
+
+    print(p1.Name)
+    print(p2.Name)
+    
+
+if __name__ == '__main__':
+    main()
